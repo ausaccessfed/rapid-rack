e25dece379147cf51a6ae4d18f4f59af14acdaa5
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2014-12-06 08:44:25 +1000

    Add TestAuthenticator to assist Capybara examples
    Login via the existing mechanism is quite messy in Capybara examples,
    because the redirect to Rapid Connect can't be trapped and stubbed out.
    
    This provides a hook and an alternate code path so that test code can
    inject a JWT which will be presented with a simple 'Login' button to
    click in test cases.

--- lib/rapid_rack.rb
+++ lib/rapid_rack.rb
@@ -3,6 +3,7 @@ end
 
 require 'rapid_rack/version'
 require 'rapid_rack/authenticator'
+require 'rapid_rack/test_authenticator'
 require 'rapid_rack/default_receiver'
 require 'rapid_rack/redis_registry'
 require 'rapid_rack/engine' if defined?(Rails)
